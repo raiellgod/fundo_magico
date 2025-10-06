db1517544293225ef4017f24963ba0c9c5c7d2af
Raiel Landre Godinho <raiellgod@gmail.com>
2025-10-05 21:16:17 -0300

    Aula 3

---
+++ src/js/script.js
@@ -0,0 +1,68 @@
+function setLoading(isLoading) {
+	const btnSpan = document.getElementById("generate-btn");
+
+	if (isLoading) {
+		btnSpan.innerHTML = "Gerando Backgroud...";
+	} else {
+		btnSpan.innerHTML = "Gerar Background Mágico";
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+	
+
+	const form = document.querySelector(".form-group");
+	const textArea = document.getElementById("description");
+	const htmlCode = document.getElementById("html-code");
+	const cssCode = document.getElementById("css-code");
+	const preview = document.getElementById("preview-section");
+
+	form.addEventListener("submit", async function (event) {
+		event.preventDefault();
+
+		
+		const description = textArea.value.trim();
+
+		if (!description) {
+			return;
+		}
+
+		
+		setLoading(true);
+
+		
+		try {
+			const response = await fetch("https://devclub-n8n-editor.g4yfpm.easypanel.host/webhook-test/gerador-fundo", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ description }),
+			});
+
+			const data = await response.json();
+			htmlCode.textContent = data.code || "";
+			cssCode.textContent = data.style || "";
+
+			preview.style.display = "block";
+			preview.innerHTML = data.code || "";
+
+			let styleTag = document.getElementById("dynamic-style");
+
+			if (styleTag) styleTag.remove();
+
+			if (data.style) {
+				styleTag = document.createElement("style");
+				styleTag.id = "dynamic-style";
+
+				styleTag.textContent = data.style;
+				document.head.appendChild(styleTag);
+			}
+		} catch (error) {
+			console.error("Erro ao gerar o fundo:", error);
+			htmlCode.textContent = "Não consegui gerar o código HTML, tente novamente";
+			cssCode.textContent = "Não consegui gerar o código CSS, tente novamente";
+			preview.innerHTML = "";
+		} finally {
+			setLoading(false);
+		}
+	});
+});
